70981366a540805cdf12c1be9a18eb7873434670
kirill3005 <136170294+kirill3005@users.noreply.github.com>
2025-03-15 10:28:06 +0300

    Update playwright_async.py

--- playwright_async.py
+++ playwright_async.py
@@ -8,7 +8,7 @@ async def fetch_product_ids(session, category_name, page_num):
     url = f"https://search.wb.ru/exactmatch/ru/common/v9/search?ab_testing=false&appType=1&curr=rub&dest=123586167&lang=ru&page={page_num}&query={category_name}&resultset=catalog&sort=popular&spp=30&suppressSpellcheck=false"
     try:
         async with session.get(url, timeout=10) as response:
-            data = await response
+            data = response
             print(data)
             data = data.json()
             return [product['id'] for product in data.get('data', {}).get('products', [])]
